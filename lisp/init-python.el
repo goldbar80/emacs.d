64ed289084d0935562f10a2ece76ece761cb9ba8
Jinha Kim <jinha.kim@oracle.com>
2018-05-17 00:13:22 -0700

    [update] use elpy

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,18 +1,26 @@
-(setq auto-mode-alist
-      (append '(("SConstruct\\'" . python-mode)
-                ("SConscript\\'" . python-mode))
-              auto-mode-alist))
+;; (setq auto-mode-alist
+;;       (append '(("SConstruct\\'" . python-mode)
+;;                 ("SConscript\\'" . python-mode))
+;;               auto-mode-alist))
 
-(require-package 'pip-requirements)
+;; (require-package 'pip-requirements)
 
-(when (maybe-require-package 'anaconda-mode)
-  (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (add-hook 'python-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+;; (when (maybe-require-package 'anaconda-mode)
+;;   (after-load 'python
+;;     (add-hook 'python-mode-hook 'anaconda-mode)
+;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;;   (when (maybe-require-package 'company-anaconda)
+;;     (after-load 'company
+;;       (add-hook 'python-mode-hook
+;;                 (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
+(require-package 'elpy)
+(require-package 'flycheck)
+
+(elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (provide 'init-python)
