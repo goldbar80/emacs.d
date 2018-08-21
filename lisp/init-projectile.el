e904de002b382c64eb63a1f04c6830cc2d1e48e2
Jinha Kim <jinha.kim@oracle.com>
2018-08-21 13:12:53 -0700

    [fix] projectile - configure prefix-key (?)

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,6 +1,9 @@
 (when (maybe-require-package 'projectile)
   (add-hook 'after-init-hook 'projectile-mode)
 
+  (after-load 'projectile
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
   ;; The following code means you get a menu if you hit "C-c p" and wait
   (after-load 'guide-key
     (add-to-list 'guide-key/guide-key-sequence "C-c p"))
@@ -13,6 +16,5 @@
        (if (file-remote-p default-directory)
            " Proj"
          (format " Proj[%s]" (projectile-project-name)))))))
-
 
 (provide 'init-projectile)
